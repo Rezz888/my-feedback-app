1fb4acb6ab9c18b89be185409c9e7855f0a2250f
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2022-06-03 11:36:27 -0400

    FeedbackForm component added for feedback submission

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -5,8 +5,21 @@ import Button from './Shared/Button'
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
+
     const handleTextChange = e => {
-        setText(e.target.value)
+      if (text === ''){
+        setBtnDisabled(true)
+        setMessage(null)
+      } else if (text !== '' && text.trim().length <= 10){
+        setMessage('Text must be at least 10 characters')
+        setBtnDisabled(true)
+      } else {
+        setMessage(null)
+        setBtnDisabled(false)
+      }
+      setText(e.target.value)
     }
   return (
   <Card>
@@ -20,8 +33,10 @@ function FeedbackForm() {
              placeholder='Write a review' 
              value={text}
              />
-             <Button type='submit'>Send</Button>
+             <Button type='submit' isDisabled={btnDisabled}>Send</Button>
          </div>
+
+         {message && <div className='message'>{message}</div>}
      </form>
   </Card>
   )
