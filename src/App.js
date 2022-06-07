9c2545cfc7347ce4e9183bef31887d680665116f
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2022-06-07 02:00:52 -0400

    Added onSubmit event handler in FeedbackForm.jsx to submit the feedback and the ratings. A function which takes in a parameter passed in from App.jsx to FeedbackForm.jsx in order to add feedback. Plus installed a package uuid to add id to the feedback object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { v4 as uuidv4} from 'uuid'
 import { useState } from 'react'
 import Header from "./Components/Header"
 import FeedbackData from './data/feedbackData'
@@ -8,6 +9,11 @@ import FeedbackForm from './Components/FeedbackForm'
 
 function App(){
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback)=> {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id)=> {
     if (window.confirm('Are you sure you want to delete this')){
@@ -20,7 +26,7 @@ function App(){
       <>
         <Header /> 
         <div className="container">
-            <FeedbackForm />
+            <FeedbackForm handleAdd={addFeedback}/>
             <FeedbackStats feedback={feedback} />
             <FeedbackList feedback={feedback} 
             handleDelete={deleteFeedback}/>
